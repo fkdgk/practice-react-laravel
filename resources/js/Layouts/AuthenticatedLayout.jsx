13d546dc8319bc84cbb8dec2c2b89b68b2f6574f
ShuheiFukuda <s_fukuda@keywalker.co.jp>
2023-03-26 20:43:40 +0900

    ページtitleを修正

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -111,12 +111,12 @@ export default function Authenticated({ auth, header, children }) {
                     <div className="container-fluid">
                         <div className="row mb-2">
                             <div className="col-sm-6">
-                                <h1>Blank Page</h1>
+                                <h1>{header}</h1>
                             </div>
                             <div className="col-sm-6">
                                 <ol className="breadcrumb float-sm-right">
                                     <li className="breadcrumb-item"><a href="#">Home</a></li>
-                                    <li className="breadcrumb-item active">Blank Page</li>
+                                    <li className="breadcrumb-item active">{header}</li>
                                 </ol>
                             </div>
                         </div>
