ea4b9d599edee32ab544308fb05730b19e6fe1f8
ShuheiFukuda <s_fukuda@keywalker.co.jp>
2023-03-26 21:16:38 +0900

    整形

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -1,5 +1,4 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-
 export default function Edit({ auth }) {
     return (
         <AuthenticatedLayout
@@ -7,31 +6,27 @@ export default function Edit({ auth }) {
             header='Profile'
         >
             <div class="row">
-
                 <div class="col-md-6">
-
                     <div class="card card-primary">
                         <div class="card-header">
                             <h3 class="card-title">Quick Example</h3>
                         </div>
-
-
                         <form>
                             <div class="card-body">
                                 <div class="form-group">
                                     <label for="exampleInputEmail1">Email address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" />
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputPassword1">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputFile">File input</label>
                                     <div class="input-group">
                                         <div class="custom-file">
-                                            <input type="file" class="custom-file-input" id="exampleInputFile">
-                                                <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                            <input type="file" class="custom-file-input" id="exampleInputFile" />
+                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
                                         </div>
                                         <div class="input-group-append">
                                             <span class="input-group-text">Upload</span>
@@ -39,35 +34,17 @@ export default function Edit({ auth }) {
                                     </div>
                                 </div>
                                 <div class="form-check">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
                                 </div>
                             </div>
-
-
                             <div class="card-footer">
                                 <button type="submit" class="btn btn-primary">Submit</button>
                             </div>
                         </form>
                     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
                 </div>
-
-
-
-                                </div>
+            </div>
         </AuthenticatedLayout>
     );
 }
