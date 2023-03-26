6738cf02363daf18c4e1f588fcbabeff5ce7b496
ShuheiFukuda <s_fukuda@keywalker.co.jp>
2023-03-26 21:30:58 +0900

    ユーザ名更新用のcomponent設置

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -1,22 +1,36 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { useForm } from '@inertiajs/react'
+
 export default function Edit({ auth }) {
+
+    const { data, setData, post, processing, errors } = useForm({
+        name: '',
+    });
+
+    function submit(e) {
+        e.preventDefault()
+        post(route('profile.update'))
+    }
+
+
     return (
         <AuthenticatedLayout
             auth={auth}
             header='Profile'
         >
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card card-primary">
-                        <form>
-                            <div class="card-body">
-                                <div class="form-group">
-                                    <label for="userName">user name</label>
-                                    <input type="text" class="form-control" id="userName" placeholder="Enter user name" />
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="card card-primary">
+                        <form onSubmit={submit}>
+                            <div className="card-body">
+                                <div className="form-group">
+                                    <label htmlFor="userName">user name</label>
+                                    <input value={data.name} onChange={e => { setData(e.target.value) }} type="text" className="form-control" id="userName" placeholder="Enter user name" />
+                                    {errors.name && <div>{errors.name}</div>}
                                 </div>
                             </div>
-                            <div class="card-footer">
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                            <div className="card-footer">
+                                <button type="submit" className="btn btn-primary">Submit</button>
                             </div>
                         </form>
                     </div>
