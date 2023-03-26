3b69859e466c54c811d636810b08385fd7aa9729
ShuheiFukuda <s_fukuda@keywalker.co.jp>
2023-03-26 21:01:54 +0900

    プロフィールリンク修正

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -28,24 +28,9 @@ export default function Authenticated({ auth, header, children }) {
                                 <i className="far fa-bell"></i> {auth.user.name}
                             </a>
                             <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                                <span className="dropdown-item dropdown-header">15 Notifications</span>
+                                <Link className='dropdown-item' href={route('profile.edit')}><i className="fas fa-user mr-2"></i> profile</Link>
                                 <div className="dropdown-divider"></div>
-                                <a href="#" className="dropdown-item">
-                                    <i className="fas fa-envelope mr-2"></i> 4 new messages
-                                    <span className="float-right text-muted text-sm">3 mins</span>
-                                </a>
-                                <div className="dropdown-divider"></div>
-                                <a href="#" className="dropdown-item">
-                                    <i className="fas fa-users mr-2"></i> 8 friend requests
-                                    <span className="float-right text-muted text-sm">12 hours</span>
-                                </a>
-                                <div className="dropdown-divider"></div>
-                                <a href="#" className="dropdown-item">
-                                    <i className="fas fa-file mr-2"></i> 3 new reports
-                                    <span className="float-right text-muted text-sm">2 days</span>
-                                </a>
-                                <div className="dropdown-divider"></div>
-                                <a href="#" className="dropdown-item dropdown-footer">See All Notifications</a>
+                                <Link as='button' method='post' href={route('logout')} className='dropdown-item dropdown-footer'>LogOut</Link>
                             </div>
                         </li>
                     </ul>
