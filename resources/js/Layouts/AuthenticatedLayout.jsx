e0d267c1d36dfb1e1ee87faed829d6d8bb72b3e3
ShuheiFukuda <s_fukuda@keywalker.co.jp>
2023-03-26 20:01:10 +0900

    コンテンツ修正

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -127,43 +127,16 @@ export default function Authenticated({ auth, header, children }) {
                     </div>
                 </section>
 
-
                 <section className="content">
-
-
-                    <div className="card">
-                        <div className="card-header">
-                            <h3 className="card-title">Title</h3>
-
-                            <div className="card-tools">
-                                <button type="button" className="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                                    <i className="fas fa-minus"></i>
-                                </button>
-                                <button type="button" className="btn btn-tool" data-card-widget="remove" title="Remove">
-                                    <i className="fas fa-times"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div className="card-body">
-                            Start creating your amazing application!
-                        </div>
-
-                        <div className="card-footer">
-                            Footer
-                        </div>
-
-                    </div>
-
-
+                    content
                 </section>
-
             </div>
 
             <footer className="main-footer">
                 <div className="float-right d-none d-sm-block">
                     <b>Version</b> 3.2.0
                 </div>
-                <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+                <strong>Copyright &copy; 2014-2021 company name.</strong> All rights reserved.
             </footer>
             <aside className="control-sidebar control-sidebar-dark">
             </aside>
