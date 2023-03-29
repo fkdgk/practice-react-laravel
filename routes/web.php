5900c967cc26b40b870d7fae9634c4997bf819f2
福田修平 <s_fukuda@keywalker.co.jp>
2023-03-30 08:32:48 +0900

    HomeControllerを追加

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\User;
 use App\Http\Controllers\Profile;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::middleware('auth')->group(function () {
     Route::get('/user/index', User\IndexController::class)->name('user.index');
 
-    Route::get('/', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+    Route::get('/', HomeController::class)->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::post('/profile/update',Profile\UpdateController::class)->name('profile.update');
